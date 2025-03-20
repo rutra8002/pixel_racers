673e7afa383f33eb9794a5152a35d61e2df8870a
SURFACE <malyszektobiasz@gmail.com>
2025-03-20 12:31:27 +0100

    Fixed teleportation when wong wai

--- app/display.py
+++ app/display.py
@@ -300,10 +300,10 @@ class game_display(basic_display):
                 else:
                     self.obstacles.append(obstacle.Obstacle(self, p[0], p[1], "brama", p[3]))
                     self.deadBramas.remove(p)
-
-        if self.wong_way and time.time() - self.p.wong_way_timer >= 3:
-            self.p.return_to_last_checkpoint()
-            self.wong_way = False
+        #
+        # if self.wong_way and time.time() - self.p.wong_way_timer >= 3:
+        #     # self.p.return_to_last_checkpoint()
+        #     self.wong_way = False
 
         if self.p.stunned and time.time() - self.p.stunned_timer >= 1:
             self.p.stunned = False
