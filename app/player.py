975dd9b4a642b28d308a4096c656ffbba07b7252
ruter <arturkummer08@gmail.com>
2025-03-26 19:18:34 +0100

    bugfix i think?

--- app/player.py
+++ app/player.py
@@ -7,7 +7,7 @@ import math
 class Player(Car):
     def __init__(self, display, coordinates, rotation, model):
         super().__init__(display, coordinates, rotation, isPlayer=True, model=model)
-        self.player_name = 'jeff'
+        self.player_name = getattr(display.game, 'player_name', 'jeff')
         self.display.game.player_name = self.player_name
         self.wong_way_timer = 0
 
