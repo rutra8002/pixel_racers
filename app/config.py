b18f3c7e32d6f4d544a47a2b8580a950560ed24d
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2025-03-31 22:19:21 +0200

    disabled debug by default

--- app/config.py
+++ app/config.py
@@ -37,7 +37,7 @@ def generate_default_config_data(version):
         'fps': 60,
         'title': 'Pixel racers',
         'full-screen': 1,
-        'enable_debug': 1
+        'enable_debug': 0
     }
 
 
